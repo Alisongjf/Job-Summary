9a8aa597f8e4231ae54c22903ec1f754fc519d66
SJF <1915145111@qq.com>
2019-11-22 16:24:37 +0800

    创建，提交request_process项目

---
+++ request_process/src/main/java/jianfang/song/request_process/RequestProcessApplication.java
@@ -0,0 +1,13 @@
+package jianfang.song.request_process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestProcessApplication.class, args);
+    }
+
+}
---
+++ request_process/src/test/java/jianfang/song/request_process/RequestProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package jianfang.song.request_process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
